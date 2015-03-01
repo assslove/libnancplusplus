301ec23227ce77824438a7569d0b8f999b2de2f9
assslove <houbin-12@163.com>
2015-03-01 09:14:01 +0800

    add some

---
+++ test_xml.cpp
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test_xml.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2015年01月25日 20时26分09秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:	houbin , houbin-12@163.com
+ *   Organization:  XiaoNan, Inc. ShangHai CN. All rights reserved.
+ *
+ * =====================================================================================
+ */
+
+#include <libnanc++/xml_parser.h>
+
+int main(int argc, char* argv[])
+{
+	XmlParser parser;
+	return 0;
+}
